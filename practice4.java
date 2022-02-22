6bf535fd322f6fa3c1e5f174c92061a3d4a452c5
Nakshatra <nakshatrajoshi999@gmail.com>
2022-02-22 19:34:52 +0530

    loop2

--- practice4.java
+++ practice4.java
@@ -1,6 +1,14 @@
 public class practice4 {
     
     public static void main(String[] args) {
-        
+        int n = 10 ;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j <= i; j++) {
+                        for (int k = 1; k <=i-j; k++) {
+                        System.out.print(" * ");     
+                        }                   
+                    }
+                    System.out. println();
+                }    
     }
 }
