1117295260a820b61315f163f8a54a9b9a8e97ac
Nakshatra <nakshatrajoshi999@gmail.com>
2022-02-22 19:37:14 +0530

    forloop

--- .dist/solution9.java
+++ .dist/solution9.java
@@ -1,18 +1,18 @@
 import java.util.*;
 import java.io.*;
 
-class Solution9{
-    public static void main(String []argh){
+class Solution9 {
+    public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
-            int c = a ;
-            for (int j=0; j<n; j++) {
-                c += Math.pow(2, j)*b;
-                System.out.printf(" %s",c);
+            int c = a;
+            for (int j = 0; j < n; j++) {
+                c += Math.pow(2, j) * b;
+                System.out.printf(" %s", c);
             }
             System.out.println();
         }
